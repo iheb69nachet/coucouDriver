43f5885cec936c80a5c11f9cd46e1e9cd3e72772
unknown <inachet3@gmail.com>
2021-02-11 13:03:23 +0100

    updated

---
+++ App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import AppContainer from './src/navigation/AppNavigator';
+import {Asset} from 'expo-asset';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+import * as firebase from 'firebase'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBn0_Q2-Mx4ZxgG-HnvpwT7QQJJSKTSqDo",
+  authDomain: "driver-api-71f02.firebaseapp.com",
+  databaseURL: "https://driver-api-71f02.firebaseio.com",
+  projectId: "driver-api-71f02",
+  storageBucket: "driver-api-71f02.appspot.com",
+  messagingSenderId: "31183956223",
+  appId: "1:31183956223:web:f880577c66fe7c5867c4ca"
+};
+firebase.initializeApp(firebaseConfig);
+
+export default class App extends React.Component {
+
+  state = {
+    assetsLoaded: false,
+  };
+
+  constructor(){
+    super();
+    console.disableYellowBox = true;
+  }
+
+//resource load at the time of app loading
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require('./assets/images/background.png'),
+        require('./assets/images/logo.png'),
+      ]),
+      Font.loadAsync({
+        'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+        'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+        'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+        'Roboto-Light': require('./assets/fonts/Roboto-Light.ttf'),
+      }),
+    ]);
+  };
+  
+  render() {
+    return (
+        this.state.assetsLoaded ? 
+          <AppContainer/>
+          :         
+          <AppLoading
+            startAsync={this._loadResourcesAsync}
+            onFinish={() => this.setState({ assetsLoaded: true })}
+            onError={console.warn}
+            autoHideSplash={true}
+          />
+    );
+  }
+}
---
+++ src/common/key.js
@@ -0,0 +1,1 @@
+export const google_map_key = 'AIzaSyAYwVlK3dLvNxZaSrme9y7bVgKwHppC1Oo';
